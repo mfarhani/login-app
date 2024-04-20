7f2e1cec8c8808a9fa85389d578e252f36209c33
mostafa.farhani <mostafa.farhani@247pro.com>
2024-04-20 19:35:20 +0300

    + Task has done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'login-app';
+  loginForm = new FormGroup({
+    username: new FormControl('', { validators: [Validators.required, Validators.email] }),
+    password: new FormControl('', Validators.required)
+  });
+  success?: boolean;
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login(): void {
+    const data = new URLSearchParams();
+    data.set('username', this.loginForm?.value?.username as string);
+    data.set('password', this.loginForm?.value?.password as string);
+    data.set('grant_type', 'password');
+    data.set('client_id', 'GeneralAgent');
+    const options = {
+      headers: new HttpHeaders().set(
+        'Content-Type',
+        'application/x-www-form-urlencoded'
+      )
+    };
+    this.http.post('/token', data.toString(), options).subscribe({
+      next: res => {
+        console.log(res);
+        this.success = true;
+      },
+      error: err => {
+        console.log(err);
+        this.success = false;
+      }
+    });
+  }
 }
+
+
